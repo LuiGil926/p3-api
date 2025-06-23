30bca069bf5321dc27a884f5b1cf3ff159625c44
LuiGil926 <abreuleida327@gmail.com>
2025-06-22 21:11:46 -0400

    update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login, register, getproducts, createCart } from '../controllers/controles.js';
+import { login, register, getproducts, createCart, getCart, updateCart, addCart } from '../controllers/controles.js';
 
 const router = Router();
 
@@ -7,5 +7,9 @@ router.post('/login', login);
 router.get('/products', getproducts);
 router.post('/register', register);
 router.post('/createCart', createCart);
+router.post('/getCart', getCart);
+router.post('/updateCart', updateCart);
+router.post('/addCart', addCart);
+
 
 export default router;
